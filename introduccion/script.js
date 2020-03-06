97a2b3858fcb98e5c5c2695497b706b6fe144260
cchura94 <cchura.cpc@gmail.com>
2020-03-06 17:23:55 -0400

    introducción y estructuras en javascript

---
+++ introduccion/script.js
@@ -0,0 +1,9 @@
+alert("Hola desde el archivo externo");
+var n1 = prompt("Ingrese numero 1");
+var n2 = prompt("Ingrese numero 2");
+var suma = parseInt(n1)  + parseInt(n2);
+
+alert("La suma es: "+ suma);
+
+console.log(typeof(n1));
+console.log(typeof(suma));
